ebd2d69a608f5781373e58811cf89166cfad26a6
RavenTamer <jeremias.furrer@hotmail.com>
2026-01-12 11:21:19 +0100

    wip: frontend

---
+++ src/frontend/namepicker/app/pages/classManager.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Class Manager</h2>
+</template>
---
+++ src/frontend/namepicker/app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="namePickerContainer">
+        <select name="schoolClasses" id="schoolClasses" v-bind="selectedSchoolClass">
+            <option v-for="schoolClass in schoolClasses">{{ schoolClass.name }}</option>
+        </select>
+        <br>
+
+        <input type="number" name="nameCount" id="nameCount" v-bind="nameCount">
+        <br>
+
+        <button @click="onPick">Picken</button>
+    </div>
+</template>
+
+<script>
+import "~/assets/css/style.css"
+
+const nameCount = ref(0)
+const selectedSchoolClass = ref("")
+const schoolClasses = ref(["class1", "class2"])
+
+const onPick = () => {
+    return ""
+}
+</script>
